41a2850997adaf1b2788636c064d5e87b905757d
Maciek8848 <mac.zeb24@wp.pl>
2024-01-08 11:06:45 +0100

    poprawki do platformy

--- TheGame/Assets/Scripts/stickToPlatform.cs
+++ TheGame/Assets/Scripts/stickToPlatform.cs
@@ -6,17 +6,10 @@ public class stickToPlatform : MonoBehaviour
 {
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.name == "Player")
-        {
-            collision.gameObject.transform.SetParent(transform);
-        }
+        collision.gameObject.transform.SetParent(transform);
     }
     private void OnTriggerExit(Collider collision)
     {
-        if (collision.gameObject.name == "Player")
-        {
             collision.gameObject.transform.SetParent(null);
-        }
-
     }
 }
